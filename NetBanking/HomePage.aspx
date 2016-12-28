d98557225a08343cc97e3633db78d2034d5f1fbd
Rakesh <mailtograkesh@gmail.com>
2016-12-28 22:40:57 +0530

    added client pages

---
+++ NetBanking/HomePage.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/sbi-Admin.master" AutoEventWireup="true" CodeFile="HomePage.aspx.cs" Inherits="HomePage" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+    <style type="text/css">
+    .auto-style1 {
+        width: 100%;
+    }
+</style>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table class="auto-style1">
+    <tr>
+        <td>
+            <asp:Image ID="Image2" runat="server" ImageUrl="~/images/image1.jpg" />
+        </td>
+        <td>
+            <asp:Image ID="Image4" runat="server" ImageUrl="~/images/image3.jpg" />
+        </td>
+        <td>
+            <asp:Image ID="Image6" runat="server" ImageUrl="~/images/image4.jpg" />
+        </td>
+        <td>
+            <asp:Image ID="Image5" runat="server" ImageUrl="~/images/image5.jpg" />
+        </td>
+        <td>
+            <asp:Image ID="Image3" runat="server" ImageUrl="~/images/image2.jpg" />
+        </td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+    <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+</table>
+</asp:Content>
+
--- NetBanking/NewUser.aspx.cs
+++ NetBanking/NewUser.aspx.cs
@@ -9,6 +9,87 @@ public partial class NewUser : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        BindDDL("Year");
+        BindDDL("Month");
+        BindDDL("Date");
+    }
+    private void BindDDL(string ddl)
+    {
+        switch (ddl)
+        {
+            case "Year":
+                ddlYear.DataSource = GetYear();
+                ddlYear.DataBind();
+                ddlYear.Items.Insert(0, new ListItem("Year"));
+                break;
+            case "Date":
+                ddlDate.DataSource = GetDate();
+                ddlDate.DataBind();
+                ddlDate.Items.Insert(0, new ListItem("Date"));
+                break;
+            case "Month":
+                ddlMonth.DataSource = GetMonth();
+                ddlMonth.DataBind();
+                ddlMonth.Items.Insert(0, new ListItem("Month"));
+                break;
+        }
+    }
+    private List<int> GetYear()
+    {
+        List<int> year = new List<int>();
+        for (int i = (DateTime.Now.Year) - 20; i != (DateTime.Now.Year) + 20; i++)
+        {
+            year.Add(i);
+        }
+        return year;
 
+    }
+    private List<int> GetDate()
+    {
+        List<int> date = new List<int>();
+        for (int i = 1; i <= 31; i++)
+        {
+            date.Add(i);
+        }
+        return date;
+    }
+    private List<int> GetMonth()
+    {
+        List<int> month = new List<int>();
+        for (int i = 1; i <= 12; i++)
+        {
+            month.Add(i);
+        }
+        return month;
+    }
+
+    protected void CustomValidator1_ServerValidate(object sender, ServerValidateEventArgs e)
+    {
+        CustomValidator validator = (CustomValidator)sender;
+
+        ddlDate = (DropDownList)validator.Parent.FindControl("ddlDate");
+        ddlMonth = (DropDownList)validator.Parent.FindControl("ddlMonth");
+        ddlYear = (DropDownList)validator.Parent.FindControl("ddlYear");
+
+        if (ddlDate.SelectedIndex == 0 || ddlMonth.SelectedIndex == 0 ||
+            ddlYear.SelectedIndex == 0)
+        {
+            e.IsValid = false;
+        }
+        else
+        {
+            string dateOfBirthString = ddlDate.SelectedItem.Value + "/" + ddlMonth.SelectedItem.Value +
+                                       "/" + ddlYear.SelectedItem.Value;
+            try
+            {
+                System.Globalization.CultureInfo cultureinfo = new System.Globalization.CultureInfo("in-IN");
+
+                DateTime.Parse(dateOfBirthString, cultureinfo);
+            }
+            catch
+            {
+                e.IsValid = false;
+            }
+        }
     }
 }
